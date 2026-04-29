b6d46fd2e4094278849dd5e42d0b1ef45b0fd550
Teniolaaa-codes <agboolat765@gmail.com>
2026-04-29 16:14:43 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+let btn = document.querySelector("button");
+let adviceId = document.querySelector("#advice-id");
+let adviceText = document.querySelector("#advice-text");
+
+btn.addEventListener("click", async () => {
+//   btn.classList.toggle("animate-pulse");
+  btn.classList.toggle("rotate-0");
+  btn.classList.toggle("rotate-180");
+
+  let res = await fetch("https://api.adviceslip.com/advice");
+  let data = await res.json();
+
+  adviceId.textContent = `ADVICE #${data.slip.id}`;
+  adviceText.textContent = `"${data.slip.advice}"`;
+});
